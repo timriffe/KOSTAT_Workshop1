013ab30585adbd38dec245e51207f4ff08bc9c21
timriffe <tim.riffe@gmail.com>
2022-08-02 09:55:47 +0900

    script for students to source to complete the lifetables

---
+++ 04_lifetable_functions.R
@@ -0,0 +1,59 @@
+
+# This R script contains just the simple lifetable identity functions that
+# we made in class on Monday 1 Aug, 2022. I made some minor changes to
+# 1) simplify function names
+# 2) handle the open age group in some cases
+
+# NEW name
+get_nqx <- function(nMx, nAx, n){
+  nqx <- (n * nMx) / ( 1 + (n - nAx) * nMx)
+  
+  # these are called "kludge"s
+  nqx[nqx > 1] <- 1
+  nqx[nqx < 0] <- 0
+  
+  # NEW close out with 1 
+  nqx[length(nqx)] <- 1
+  
+  return(nqx)
+}
+
+# NEW name
+get_lx <- function(nqx, radix){
+  # cumulative product of the conditional
+  # survival probabilities in each age.
+  lx <- cumprod(1 - nqx)
+  lx <- c(1, lx)
+  lx <- lx[-length(lx)]
+  lx <- radix * lx
+  return(lx)
+}
+# NEW name
+get_ndx <- function( nqx, lx){
+  nqx * lx
+}
+
+# NEW a second version of get_ndx() according to 
+# student petition
+get_ndx_2 <- function( nqx, lx){
+  -diff(c(lx, 0))
+}
+
+# NEW name 
+get_Lx <- function(lx, nAx, ndx, n){
+  nLx      <- n * lx - (n - nAx) * ndx
+  
+  # NEW care for closeout
+  N        <- length(nLx)
+  nLx[N]	 <- lx[N] * nAx[N] 
+}
+
+# same
+get_Tx <- function(nLx){
+  nLx %>% rev() %>% cumsum() %>% rev()
+}
+
+# same
+get_ex <- function(Tx, lx){
+  Tx / lx
+}
