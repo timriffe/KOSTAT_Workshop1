084e24635bd162da71a2cabc5afaa8282a72b40b
timriffe <tim.riffe@gmail.com>
2025-06-27 07:22:34 +0900

    adds ax approximation

--- lifetable_functions2025.R
+++ lifetable_functions2025.R
@@ -12,6 +12,19 @@ mx_to_qx <- function(mx, ax){
   return(qx)
 }
 
+mx_to_ax <- function(mx, age, sex){
+  # this copied in from session 4, why do this more than once?
+  ax = case_when(
+    sex == "male" & age == 0 & between(mx, 0, 0.02300) ~ 0.14929 - 1.99545 * mx,
+    sex == "male" & age == 0 & between(mx, 0.02300001, 0.08307) ~ 0.02832 + 3.26021 * mx,
+    sex == "male" & age == 0 & between(mx, 0.08307001, 1) ~ 0.29915,
+    sex == "female" & age == 0 & between(mx, 0, 0.01724) ~ 0.14903 - 2.05527 * mx,
+    sex == "female" & age == 0 & between(mx, 0.01724001, 0.06891) ~ 0.04667 + 3.88089 * mx,
+    sex == "female" & age == 0 & between(mx, 0.06891001,1) ~ 0.31411,
+    age == max(age) ~ 1 / mx, # constant mortality for closeout (the mean survival time)
+    TRUE ~ .5                 # if subject to constant mortality is the inverse of the rate
+  )
+}
 
 # convert qx to lx
 qx_to_lx <- function(qx, radix = 1e5){
